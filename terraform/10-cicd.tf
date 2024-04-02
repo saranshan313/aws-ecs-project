fe46cef0c8b6992d6f123c8e17dfd5a5614162b8
saranshan313 <sar.mca05@gmail.com>
2024-04-02 19:07:29 +1100

    update codedeploy role

--- terraform/10-cicd.tf
+++ terraform/10-cicd.tf
@@ -151,11 +151,9 @@ data "aws_iam_policy_document" "ecs_codedeploy_assume_policy" {
   }
 }
 
-resource "aws_iam_role_policy" "ecs_codedeploy_permission_policy" {
-  role = aws_iam_role.ecs_codebuild.name
-  policy = [
-    "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
-  ]
+resource "aws_iam_role_policy_attachment" "ecs_codedeploy_permission_policy" {
+  role      = aws_iam_role.ecs_codedeploy.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
 
 resource "aws_iam_role" "ecs_codedeploy" {
