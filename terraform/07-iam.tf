f6afea5b9f8512375c04138816af8518fb814cea
saranshan313 <sar.mca05@gmail.com>
2024-04-17 23:14:04 +1000

    update ecs task exec role permission

--- terraform/07-iam.tf
+++ terraform/07-iam.tf
@@ -23,3 +23,25 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
+
+resource "aws_iam_policy" "ecsTaskExecutionRole_policy" {
+  name        = "policy-${local.settings.env}-${local.settings.region}-ecstask-01"
+  path        = "/"
+  description = "Allow ECS task to access the Secrets"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue",
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_secretsmanager_secret.ecs_rds.arn}"
+      },
+    ]
+  })
+}
+
